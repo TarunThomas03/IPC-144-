60c57257763af7505b8b6c6b53e01f770f2930e1
Tarun Thomas <134177814+TarunThomas03@users.noreply.github.com>
2023-08-17 19:12:38 -0400

    Update clinic.h

--- source/repos/ipc144/ms2/clinic.h
+++ source/repos/ipc144/ms2/clinic.h
@@ -1,15 +1,5 @@
 #pragma once
-/*/////////////////////////////////////////////////////////////////////////
-                        Assignment 1 - Milestone 2
-Full Name  : Tarun Thomas
-Student ID#: 113605224
-Email      : tthomas60@myseneca.ca
-Section    : NFF
-Authenticity Declaration:
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-/////////////////////////////////////////////////////////////////////////*/
+
 
 
 // SAFE-GUARD: 
